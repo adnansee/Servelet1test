5c88e1741401f92ba2cf9b763f50e0a6dadb2845
Eddy <adnansee@gmail.com>
2019-08-30 12:00:43 +0200

    try

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -4,14 +4,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
-@WebServlet("HelloWorld")
+@WebServlet("")
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
         resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        try(PrintWriter out = resp.getWriter()){
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title> HELLO MATE SERVLET </title>");
+            out.println("</head>");
+            out.println("</body>");
+            out.println("HELLO SERV&&&");
+            out.println("</html>");
+            out.println();
+
+        }
     }
 }
